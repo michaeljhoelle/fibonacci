f2046db3ca845717f833468bbf766fd918ea0a3c
Mike <michaeljhoelle@gmail.com>
2024-01-18 17:30:48 -0500

    Add functionality

--- src/main/java/com/m3/fibonacci/fibonacci/controller/FibonacciController.java
+++ src/main/java/com/m3/fibonacci/fibonacci/controller/FibonacciController.java
@@ -1,2 +1,29 @@
-package com.m3.fibonacci.fibonacci.controller;public class FibonacciController {
+package com.m3.fibonacci.fibonacci.controller;
+
+import com.m3.fibonacci.fibonacci.service.FibonacciService;
+import lombok.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/fibonacci")
+public class FibonacciController {
+
+    private final FibonacciService fibonacciService;
+
+    @CrossOrigin
+    @PostMapping(value = "/next", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public FibonacciResult getNextFibonacci(@RequestBody BigInteger input) {
+        return new FibonacciResult(fibonacciService.computeNextFibonacciNumber(input).toString());
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static class FibonacciResult {
+        private String value;
+    }
 }
--- src/main/java/com/m3/fibonacci/fibonacci/service/FibonacciService.java
+++ src/main/java/com/m3/fibonacci/fibonacci/service/FibonacciService.java
@@ -1,2 +1,26 @@
-package com.m3.fibonacci.fibonacci.service;public class FibonacciService {
+package com.m3.fibonacci.fibonacci.service;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+
+@Service
+public class FibonacciService {
+
+    public BigInteger computeNextFibonacciNumber(BigInteger input) {
+        if (input.compareTo(BigInteger.ONE) < 0) {
+            return BigInteger.ONE; // Default to the first fibonacci number if input is below that
+        }
+        BigInteger firstNumber;
+        BigInteger secondNumber = BigInteger.ONE;
+        BigInteger nextNumber = BigInteger.TWO;
+
+        while (nextNumber.compareTo(input) < 1) {
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+            nextNumber = firstNumber.add(secondNumber);
+        }
+        return nextNumber;
+    }
+
 }
